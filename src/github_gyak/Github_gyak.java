b1a646accf6c563cf52cde1c58651c22de134ba6
Venc03 <vegi.dani1@gmail.com>
2024-09-30 13:36:44 +0200

    merge fejlesztes to master

--- src/github_gyak/Github_gyak.java
+++ src/github_gyak/Github_gyak.java
@@ -3,6 +3,7 @@ public class Github_gyak {
     public static void main(String[] args) {
         Github_gyak obj = new Github_gyak();
         int i = obj.fv();
+        //merged
     }
 
     private int fv() {
