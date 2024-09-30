36c80d65bcf5a0965d312415250bf5774b7169a5
Venc03 <vegi.dani1@gmail.com>
2024-09-30 13:23:36 +0200

    mugokdo fkv non static

--- src/github_gyak/Github_gyak.java
+++ src/github_gyak/Github_gyak.java
@@ -1,7 +1,13 @@
 package github_gyak;
 public class Github_gyak {
     public static void main(String[] args) {
-        
+        Github_gyak obj = new Github_gyak();
+        int i = obj.fv();
+    }
+
+    private int fv() {
+        System.out.println("Nem static");
+        return = "Great"
     }
     
 }
